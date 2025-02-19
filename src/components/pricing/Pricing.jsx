ddb99592e7136043dd6b939278e0ae6f1236b259
Odusami <olatunde.odusami12@gmail.com>
2025-02-19 16:28:20 +0100

    creating pricing page

--- src/components/pricing/Pricing.jsx
+++ src/components/pricing/Pricing.jsx
@@ -1,9 +1,16 @@
-import './Pricing.scss';
+import "./Pricing.scss";
 
 const Pricing = () => {
   return (
-    <div>Pricing</div>
-  )
-}
+    <>
+      <div className="py-5">
+        <div className="container">
+          <h2>Modern software solutions for photography studios</h2>
+          <p>Get started for free. Upgrade only when needed.</p>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Pricing
+export default Pricing;
