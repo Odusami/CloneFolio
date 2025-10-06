c0a8bd36dc2123f03f418854c64e6e821bf66586
Odusami <olatunde.odusami12@gmail.com>
2025-10-06 16:31:13 +0100

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import './styles/main.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./styles/main.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/cloneFolio">
-   
-    <App />
-
+    <BrowserRouter>
+      <App />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
